7000632912d9fd8a20b561d1cfd02da1bb7cd686
Percy Osunde <posunde@my.centennialcollege.ca>
2026-05-12 00:26:15 -0400

    Initial Spring Boot project setup

---
+++ src/main/java/com/percybuilder/ecommerce/SpringbootEcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.percybuilder.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEcommerceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEcommerceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/percybuilder/ecommerce/SpringbootEcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.percybuilder.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEcommerceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
